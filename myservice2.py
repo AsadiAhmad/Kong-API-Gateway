92e57e05c124974eee961fc4f8ab3ae918de130e
Kiarash Rahmani <microkia1928@gmail.com>
2023-12-29 21:52:33 +0330

    adding second service

--- myservice2.py
+++ myservice2.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hi, I'm service 1"
+    return "Hi, I'm service 2"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8080)
+    app.run(debug=True, host='127.0.0.2', port=8080)
